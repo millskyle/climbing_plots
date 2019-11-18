dd861be2612a7abbfb562159e4ef3806f6ef9ebd
Kyle Mills <kyle@kylemills.net>
2019-11-18 16:45:10 -0500

    added transparency

--- do_plots.py
+++ do_plots.py
@@ -4,16 +4,16 @@ urllib.request.urlretrieve("http://kylemills.ca/climbing/dump.php?u=kmills&repor
 df = pd.read_csv("/tmp/out.txt")
 df['date']= pd.to_datetime(df['date']) 
  
-success_ratio(df).savefig('1.png')
-plot_cum_frac(df, attempt=0).savefig('2.png')
-plot_cum_frac(df, attempt=1).savefig('3.png')
-climbing_score(df).savefig('4.png')
+success_ratio(df).savefig('1.png', transparent=True)
+plot_cum_frac(df, attempt=0).savefig('2.png', transparent=True)
+plot_cum_frac(df, attempt=1).savefig('3.png', transparent=True)
+climbing_score(df).savefig('4.png', transparent=True)
 
 
 urllib.request.urlretrieve("http://kylemills.ca/climbing/dump.php?u=kmills", '/tmp/out.txt')
 df = pd.read_csv("/tmp/out.txt")
 df['date']= pd.to_datetime(df['date']) 
 
-scatterplotclimbs(df, attempt=1).savefig('5.png')
-scatterplotclimbs(df, attempt=0).savefig('6.png')
-climbing_trends(df).savefig('7.png')
+scatterplotclimbs(df, attempt=1).savefig('5.png', transparent=True)
+scatterplotclimbs(df, attempt=0).savefig('6.png', transparent=True)
+climbing_trends(df).savefig('7.png', transparent=True)
